fe67fc6646e57f6eadd096dfd765e2b5b6fba875
s4-hub <syafii.newbie@gmail.com>
2021-09-16 10:33:47 +0700

    edit setting

--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = config('DEBUG', default=False)
-DEBUG = True
+DEBUG = False
 
 # load production server from .env
 ALLOWED_HOSTS = ['sicode.id', 'localhost', '127.0.0.1',
@@ -88,10 +88,21 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+# 'default': {
+# 'ENGINE': 'django.db.backends.sqlite3',
+# 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+# }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',
+        'USER': 'sicm6455_admin',
+        'PASSWORD': 'Rahasia007123#$',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -146,7 +157,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
@@ -156,11 +167,11 @@ STATICFILES_DIRS = (
 #############################################################
 #############################################################
 
-# STATIC_ROOT = '/home/sicm6455/python/public/'
+STATIC_ROOT = '/home/sicm6455/python/public/'
 
 MEDIA_URL = '/media/'
-# MEDIA_ROOT = '/home/sicm6455/python/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/home/sicm6455/python/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
