ab730a1464e1d7a62b356f855a390ec3e924886c
s4-hub <syafii.newbie@gmail.com>
2021-09-19 00:50:28 +0700

    fix profile

--- klaim_registration/views.py
+++ klaim_registration/views.py
@@ -265,7 +265,7 @@ def PengkinianTK(request, pk):
     # qs = get_object_or_404(DataTK, kpj__user_kpj_id=pk)
     qs = NoKPJ.objects.select_related(
         'user_kpj').filter(user_kpj_id=pk).first()
-    if qs.user_kpj.tempat_lahir is None:
+    if qs.user_kpj.tempat_lahir == '':
         messages.WARNING(request, "Update Profile terlebih dahulu!")
         return redirect(reverse('list-kpj'))
     elif request.method == 'POST':
